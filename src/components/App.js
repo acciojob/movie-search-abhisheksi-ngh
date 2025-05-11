5b46b963f3d63e3498cc9b3c980b3037412fa50b
abhishekashj <abhishekkumrsingh9585@gmail.com>
2025-05-11 22:15:32 +0530

    fs

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-
+import './App.css';
 const App = () => {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
